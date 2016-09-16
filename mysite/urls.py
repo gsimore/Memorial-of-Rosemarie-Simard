a4eb8f024b56f01082ddbd0234d5d70d8a3aa088
gsimore <gsimardmoore@gmail.com>
2016-09-15 18:19:32 -0700

    have working urls and views, plus home.html and post_list.html ready to go on to some templating and styling :)

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import url, include
+from mewall import views
 
 urlpatterns = [
+    url(r'^$', views.home, name='home'),
     url(r'^admin/', admin.site.urls),
+    url(r'', include('mewall.urls')),
 ]
